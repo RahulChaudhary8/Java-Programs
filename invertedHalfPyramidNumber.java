0eb1ea8957066549db495a296530f5aaaeecfe7d
RahulChaudhary8 <rahulmahi831@gmail.com>
2025-01-21 14:57:25 +0530

    invertedHalfPyramidNumber

---
+++ invertedHalfPyramidNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class invertedHalfPyramidNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int N = sc.nextInt();
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
