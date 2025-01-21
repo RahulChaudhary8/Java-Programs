b3845f02c8517a9f1174a228dbbd3d2fbae41593
RahulChaudhary8 <rahulmahi831@gmail.com>
2025-01-21 16:35:06 +0530

    New Changes

--- invertedHalfPyramidNumber.java
+++ invertedHalfPyramidNumber.java
@@ -6,9 +6,9 @@ public class invertedHalfPyramidNumber {
         System.out.print("Enter a number : ");
         int N = sc.nextInt();
 
-        for (int i = N; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j+" ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <=N- i+1 ; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
