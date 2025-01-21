15f76ea8bd3f8fc11e514ac28bed5c793b42ac79
RahulChaudhary8 <rahulmahi831@gmail.com>
2025-01-21 16:27:24 +0530

    A_pattern

--- invertedHalfPyramidNumber.java
+++ invertedHalfPyramidNumber.java
@@ -8,7 +8,7 @@ public class invertedHalfPyramidNumber {
 
         for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
